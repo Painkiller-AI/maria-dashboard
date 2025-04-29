d5be0322c5932aefa5a120e552e85d2db44f4881
MPrevelato <mateusprevelato@hotmail.com>
2025-04-29 14:08:16 -0300

    feat: add get_datetime date module

---
+++ src/shared/utils/get_datetime.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+
+def obter_lista_meses() -> tuple[list[str], dict[str, int]]:
+    meses = {
+        "Janeiro": 1,
+        "Fevereiro": 2,
+        "Março": 3,
+        "Abril": 4,
+        "Maio": 5,
+        "Junho": 6,
+        "Julho": 7,
+        "Agosto": 8,
+        "Setembro": 9,
+        "Outubro": 10,
+        "Novembro": 11,
+        "Dezembro": 12,
+    }
+    return list(meses.keys()), meses
+
+
+def obter_lista_anos() -> list[int]:
+    ano_atual = datetime.now().year
+    return list(range(2024, ano_atual + 1))
+
+
+def obter_numero_mes(
+    mes_selecionado: str, meses: dict[str, int]
+) -> int:  # Usando 'dict' para tipagem nativa
+    return meses[mes_selecionado]
