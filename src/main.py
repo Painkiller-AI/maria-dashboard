916baed97483ade1569ab5d4ec3a91d14c5f2339
MPrevelato <mateusprevelato@hotmail.com>
2025-04-29 13:44:08 -0300

    chore: disable login temporarily for dev testing

--- src/main.py
+++ src/main.py
@@ -2,9 +2,13 @@ import asyncio
 
 import streamlit as st
 
-from state import app_state
-from ui.pages.login import login_page
-from ui.pages.patients_overview import patients_overview_page
+from ui.pages.cemig.patients_overview import patients_overview_page
+from ui.pages.maria.indicators import indicators_page
+
+PAGES = {
+    "CEMIG - Visão Geral dos Pacientes": patients_overview_page,
+    "Maria Saúde - Indicadores": indicators_page,
+}
 
 
 async def main():
@@ -14,10 +18,11 @@ async def main():
         page_icon="assets/maria-logo.svg",
     )
 
-    if not app_state.user:
-        await login_page()
-    else:
-        await patients_overview_page()
+    # if not app_state.user:
+    #     await login_page()
+    st.sidebar.title("Navegação")
+    selected_page = st.sidebar.radio("Escolha uma página", list(PAGES.keys()))
+    await PAGES[selected_page]()  # Chama a função da página selecionada
 
 
 if __name__ == "__main__":
